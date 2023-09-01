35df26ecc4bb6b3760a4b9e46b55ddf8e6dedb54
Jacqueline A. Wright <jacquelinew945@gmail.com>
2023-09-01 06:06:40 +0100

    add channel mapping and channel name to data

--- DataCollection/main.py
+++ DataCollection/main.py
@@ -17,30 +17,38 @@ sas_token = os.environ.get("SAS_TOKEN")
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-CHANNEL_IDS = [
-    utils.get_channel_id('CNN'),  # CNN
-    utils.get_channel_id('BBCNews'),  # BBC
-    utils.get_channel_id('aljazeeraenglish'),  # Al Jazeera English
-    utils.get_channel_id('FoxNews'),  # Fox News
-    utils.get_channel_id('guardiannews'),  # The Guardian
-    utils.get_channel_id('indiatoday'),  # India Today
-    'UCqnbDFdCpuN8CMEg0VuEBqA',  # New York Times
-    'UC4SUWizzKc1tptprBkWjX2Q',  # South China Morning Post
-    utils.get_channel_id('Channel4News'),  # Channel 4 News
-    utils.get_channel_id('abcnewsaustralia')  # ABC News Australia
-]
+CHANNELS = {
+    'CNN': utils.get_channel_id('CNN'),
+    'BBC News': utils.get_channel_id('BBCNews'),
+    'Al Jazeera English': utils.get_channel_id('aljazeeraenglish'),
+    'Fox News': utils.get_channel_id('FoxNews'),
+    'The Guardian': utils.get_channel_id('guardiannews'),
+    'India Today': utils.get_channel_id('indiatoday'),
+    'New York Times': 'UCqnbDFdCpuN8CMEg0VuEBqA',
+    'South China Morning Post': 'UC4SUWizzKc1tptprBkWjX2Q',
+    'Channel 4 News': utils.get_channel_id('Channel4News'),
+    'ABC News Australia': utils.get_channel_id('abcnewsaustralia'),
+    'ABC News': utils.get_channel_id('ABCNews'),
+    'NBC News': utils.get_channel_id('NBCNews')
+}
 
 all_videos = []
-# for each channel id, get 10 of the latest videos by id and title
-for channel_id in CHANNEL_IDS:
-    all_videos.extend(utils.get_videos_from_channel(channel_id))
+
+# for each channel name and id, get 10 of the latest videos by id and title
+for channel_name, channel_id in CHANNELS.items():
+    videos_for_channel = utils.get_videos_from_channel(channel_id)
+    for video in videos_for_channel:
+        video['channel_name'] = channel_name  # append the channel name to the video data
+    all_videos.extend(videos_for_channel)
 
 all_comments = []
-# for each video extracted, get 100 comments and append the video title
+
+# for each video extracted, get 100 comments and append the video title and channel name
 for video in all_videos:
     comments_for_video = utils.get_comments(video['video_id'])
     for comment in comments_for_video:
         comment['video_title'] = video['title']  # append the video title to the comment data
+        comment['channel_name'] = video['channel_name']  # append the channel name to the comment data
     all_comments.extend(comments_for_video)
 
 
